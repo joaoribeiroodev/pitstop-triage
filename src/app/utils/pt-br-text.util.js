e95a38997f5b71b26e1527cc5413c3143ff6a6d4
João Pedro Lima Ribeiro <jpeddro0706@gmail.com>
2026-07-11 23:03:13 -0300

    chore: ignora arquivos .js compilados (compilados no build da Vercel)

--- src/app/utils/pt-br-text.util.js
+++
@@ -1,8 +0,0 @@
-import { corrigirOrtografiaPtBr, corrigirValorProfundo, normalizarTextoPdf } from './pt-br-ortografia';
-export { corrigirOrtografiaPtBr, corrigirValorProfundo, normalizarTextoPdf };
-export function corrigirDiagnosticoCdp(diagnostico) {
-    return corrigirValorProfundo(diagnostico);
-}
-export function corrigirRefinamentoResposta(resposta) {
-    return corrigirValorProfundo(resposta);
-}
